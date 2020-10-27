a506ce979527dcbdfad238c17dcd1b2ce2bfc164
Mai Pham <maitphm@gmail.com>
2020-10-27 05:19:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,6 +5,7 @@ def loop_message_five_times(string)
   while count < 5 do 
     puts string 
     count += 1 
+  end 
 end 
 
 def loop_message_n_times(string, n)
@@ -12,11 +13,21 @@ def loop_message_n_times(string, n)
   while count < n do 
     puts string 
     count += 1
+  end 
 end 
 
 def output_array(array)
-  
+  count = 0 
+  while count < array.length do 
+    puts array[count]
+    count += 1 
+  end 
 end 
 
 def return_string_array(arr)
+  count = 0 
+  while count < array.length do 
+    array[count].to_s
+    count += 1 
+  end 
 end 
