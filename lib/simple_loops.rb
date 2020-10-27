7e8e98317103220be0282896a3fcb734aedc6b97
Mai Pham <maitphm@gmail.com>
2020-10-27 05:21:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,10 +24,10 @@ def output_array(array)
   end 
 end 
 
-def return_string_array(arr)
+def return_string_array(array)
   count = 0 
-  while count < arr.length do 
-    arr[count].to_s
+  while count < array.length do 
+    array[count] = array[count].to_s
     count += 1 
   end 
 end 
